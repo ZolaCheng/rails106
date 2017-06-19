ecba162ee0747bf49987bdf0ddf713292d6710d5
Zola Chen <zolacheng@gmail.com>
2017-06-19 21:32:23 +0800

    implement post create function

---
+++ db/migrate/20170619131207_rename_user_to_user_id.rb
@@ -0,0 +1,5 @@
+class RenameUserToUserId < ActiveRecord::Migration[5.0]
+  def change
+    rename_column :posts, :user, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619103810) do
+ActiveRecord::Schema.define(version: 20170619131207) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170619103810) do
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.integer  "group_id"
-    t.integer  "user"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
